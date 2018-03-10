a55f4d30ebe0cef672cef189df3b47b2e8c66b21
WooSung Choi <johnwschoi@gmail.com>
2018-03-09 19:27:32 -0800

    More styling

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = 9000;
+const port = process.env.PORT || 9000;
 
 app.use(morgan('dev'));
 app.use('/biz/:bizId', express.static(path.join(__dirname, 'public')));
